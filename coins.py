eee383b6f521afb6eea458c05b593686e0c366ec
Jesse White <jsperlingwhite@gmail.com>
2017-04-16 22:48:12 -0700

    Update coins.py
    fixed comments

--- coins.py
+++ coins.py
@@ -5,12 +5,12 @@ from itertools import permutations
 
 coins = [2,3,5,7,9]
 
-# takes a list of coins, ls, and a fragmented
-	
+# check if the current permutation solves the equation
 def trySolution(p):
 	a,b,c,d,e = p[0],p[1],p[2],p[3],p[4]
 	return a + (b*(c**2)) + d**3 - e == 399
 
+# iterates through permutations of coins until it finds a solution
 def main():
 	perms = permutations(coins)
 	for p in perms:
